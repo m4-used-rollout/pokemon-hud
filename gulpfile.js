092b958401882058c2cdd243c440508740765674
m4-used-rollout <dave.was.r@gmail.com>
2019-05-25 23:50:56 +0000

    Updated to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -12,53 +12,7 @@ var dexNavProject = ts.createProject('src-dexnav/tsconfig.json');
 var srvProject = ts.createProject('src-server/tsconfig.json');
 var appProject = ts.createProject('src-app/tsconfig.json');
 
-gulp.task('build', ['package-app']);
-
-gulp.task('build-hud', ['build-backend'], () => hudProject.src().pipe(sourcemaps.init()).pipe(hudProject()).js.pipe(sourcemaps.write()).pipe(gulp.dest("bin/")));
-
-gulp.task('build-dexnav', ['build-backend'], () => dexNavProject.src().pipe(sourcemaps.init()).pipe(dexNavProject()).js.pipe(sourcemaps.write()).pipe(gulp.dest("bin/")));
-
-gulp.task('build-frontend', ['build-backend', 'build-hud', 'build-dexnav', 'copy-html', 'copy-img', 'copy-data', 'compile-less']);
-
-gulp.task('build-backend', () => {
-    let ts = srvProject.src().pipe(sourcemaps.init()).pipe(srvProject());
-    return merge(
-        ts.js.pipe(sourcemaps.write()).pipe(gulp.dest("bin/")),
-        ts.dts.pipe(gulp.dest("ref/"))
-    );
-});
-
-gulp.task('build-app', ['build-frontend', 'copy-config', 'copy-deps', 'copy-package'], () => appProject.src().pipe(appProject()).js.pipe(gulp.dest("bin/")));
-
-gulp.task('package-app', ['clean-old-release', 'build-app'], () => {
-    var packageJson = require('./bin/package.json');
-    gulp.src("")
-        .pipe(electron({
-            src: './bin',
-            packageJson: packageJson,
-            release: './release',
-            cache: './cache',
-            version: 'v3.0.5',
-            packaging: false,
-            // token: 'abc123...',
-            platforms: ['win32-x64'],
-            platformResources: {
-                win: {
-                    "version-string": packageJson.version,
-                    "file-version": packageJson.version,
-                    "product-version": packageJson.version,
-                    "icon": './img/pokemon.ico' //icon from http://www.iconspedia.com/icon/pokemon-icon-44128.html
-                }
-            }
-        }))
-        .pipe(gulp.dest(""));
-});
-
 gulp.task('clean-old-release', () => del('release/'));
-
-gulp.task('compile-less', ['copy-fonts'], () => gulp.src('./styles/*.less').pipe(sourcemaps.init()).pipe(less()).pipe(sourcemaps.write()).pipe(gulp.dest('bin/')));
-
-gulp.task('copy-config', ['copy-ini'], () => gulp.src('./config.json').pipe(gulp.dest('bin/')));
 
 gulp.task('copy-ini', () => gulp.src('./*.ini').pipe(gulp.dest('bin/')));
 
@@ -72,6 +26,53 @@ gulp.task('copy-img', () => gulp.src('./img/**/*').pipe(gulp.dest('bin/img/')));
 
 gulp.task('copy-package', () => gulp.src('./package.json').pipe(gulp.dest('bin/')));
 
-gulp.task('copy-deps', ['copy-node-packages'], () => gulp.src('./node_modules/react*/dist/*.js').pipe(flatten()).pipe(gulp.dest('bin/lib/')));
-
 gulp.task('copy-node-packages', () => gulp.src('./node_modules/ini/**/*').pipe(gulp.dest('bin/node_modules/ini/')));
+
+gulp.task('copy-deps', gulp.series('copy-node-packages', () => gulp.src('./node_modules/react*/dist/*.js').pipe(flatten()).pipe(gulp.dest('bin/lib/'))));
+
+gulp.task('compile-less', gulp.series('copy-fonts', () => gulp.src('./styles/*.less').pipe(sourcemaps.init()).pipe(less()).pipe(sourcemaps.write()).pipe(gulp.dest('bin/'))));
+
+gulp.task('copy-config', gulp.series('copy-ini', () => gulp.src('./config.json').pipe(gulp.dest('bin/'))));
+
+gulp.task('build-backend', () => {
+    let ts = srvProject.src().pipe(sourcemaps.init()).pipe(srvProject());
+    return merge(
+        ts.js.pipe(sourcemaps.write()).pipe(gulp.dest("bin/")),
+        ts.dts.pipe(gulp.dest("ref/"))
+    );
+});
+
+gulp.task('build-hud', gulp.series('build-backend', () => hudProject.src().pipe(sourcemaps.init()).pipe(hudProject()).js.pipe(sourcemaps.write()).pipe(gulp.dest("bin/"))));
+
+gulp.task('build-dexnav', gulp.series('build-backend', () => dexNavProject.src().pipe(sourcemaps.init()).pipe(dexNavProject()).js.pipe(sourcemaps.write()).pipe(gulp.dest("bin/"))));
+
+gulp.task('build-frontend', gulp.series('build-backend', 'build-hud', 'build-dexnav', 'copy-html', 'copy-img', 'copy-data', 'compile-less'));
+
+gulp.task('build-app', gulp.series('build-frontend', 'copy-config', 'copy-deps', 'copy-package', () => appProject.src().pipe(appProject()).js.pipe(gulp.dest("bin/"))));
+
+gulp.task('package-app', gulp.series('clean-old-release', 'build-app', () => {
+    var packageJson = require('./bin/package.json');
+    return gulp.src("./")
+        .pipe(electron({
+            src: './bin',
+            packageJson: packageJson,
+            release: './release',
+            cache: './cache',
+            version: 'v5.0.2',
+            packaging: false,
+            // token: 'abc123...',
+            platforms: ['win32-x64'],
+            platformResources: {
+                win: {
+                    "version-string": packageJson.version,
+                    "file-version": packageJson.version,
+                    "product-version": packageJson.version,
+                    "icon": './img/pokemon.ico' //icon from http://www.iconspedia.com/icon/pokemon-icon-44128.html
+                }
+            }
+        }))
+        .pipe(gulp.dest("./"));
+}));
+
+
+gulp.task('build', gulp.series('package-app'));
