ab9bf722afe0ab4868579920a928d884f216bf96
m4-used-rollout <dave.was.r@gmail.com>
2020-02-25 09:40:39 +0000

    Killscreen messages no longer in repo

--- src-dexnav/killscreen.tsx
+++ src-dexnav/killscreen.tsx
@@ -1,5 +1,7 @@
 /// <reference path="encounters.tsx" />
 /// <reference path="../src-hud/trainersprite.tsx" />
+/// <reference path="ksMessages.ts" />
+
 
 let ksMsgIndex = 0;
 
@@ -7,51 +9,11 @@ const PartyIsFainted = (party: { health: number[] }[]) => !(party || []).some(p 
 
 class KillScreen extends PersistentComponent<{ party: TPP.PartyData, enemyParty: TPP.EnemyParty }> {
     render() {
-        if (this.props.enemyParty && this.props.enemyParty.some(p=>p && p.species.national_dex == 383))
-            ksMsgIndex = 0;
+        // if (this.props.enemyParty && this.props.enemyParty.some(p=>p && p.species.national_dex == 383))
+        //     ksMsgIndex = 0;
         if (this.props.party && this.props.party.filter(p=>!!p).length > 0 && PartyIsFainted(this.props.party))
             return <div className={`encounters error`} key={`kill-screen`} data-message={ksMessages[ksMsgIndex]} />;
         ksMsgIndex = (ksMsgIndex + 1) % ksMessages.length;
         return null;
     }
 }
-
-const ksMessages = [
-    'GG IDIOTS',
-    'SYSTEM ERROR',
-    //'SHUTTING DOWN...',
-    'DISCONNECTED FROM HOST',
-    'LOSS OF SIGNAL',
-    'END OF TRANSMISSION',
-    'DESYNC',
-    'DEFEAT',
-    'MISSING IN ACTION',
-    'GAME OVER',
-    'MISSION FAILED',
-    'CHALLENGE FAILED',
-    'WASTED',
-    'PARTY DEPLETED',
-    'TRY AGAIN',
-    'TRY HARDER',
-    'QUEST FAILED',
-    // 'TEAM MAGMA WINS',
-    // 'DAD WAS RIGHT',
-    // 'NANU WAS RIGHT',
-    "THAT'S NOT SUPPOSED TO HAPPEN",
-    'GOOD EFFORT',
-    'LILLIE WAS WRONG',
-    'NICE WORK, COUSIN!',
-    'AND THERE GOES THE BATTLE!',
-    'DOWN AAAAAAAND OUT!',
-    "Y'ALL ARE STUPID",
-    'YOUR TEAM A SPLODE',
-    'TEAM WIPE',
-    'BOOOOOOOOO',
-    'BORN TO LOSE',
-    'SO SAD',
-    'WORTHLESS',
-    'PATHETIC',
-    'DISAPPOINTMENT',
-    'TOTAL LOSER',
-    'CRUSHING DEFEAT',
-];
